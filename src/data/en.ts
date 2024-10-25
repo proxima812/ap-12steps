b0037eabc924ef747e2123c15bce2e7e413445df
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-10-25 18:32:14 +0500

    1

---
+++ src/data/en.ts
@@ -0,0 +1,6 @@
+export const en = {
+	head: {
+		mainTitle: "ANONYMOUS PROCRASTINATORS - OFFICIAL SITE OF THE CIS",
+		title: "ANONYMOUS PROCRASTINATORS",
+	},
+}
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,22 +1,17 @@
 export const links = [
-	// {
-	// 	label: "Главная",
-	// 	href: "/",
-	// },
-	// {
-	// 	label: "Материалы",
-	// 	href: "#",
-	// },
 	{
 		label: "Группы",
 		href: "#groups",
+		labelEn: "Groups",
 	},
 	{
 		label: "Контакты",
+		labelEn: "Contacts",
 		href: "#contacts",
 	},
 	{
 		label: "Создать группу",
+		labelEn: "Create group",
 		href: "#create-group",
 		mdi: "mdi:creation-outline",
 	},
