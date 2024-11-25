44e9d186dea92cf5ff5b29af79ec30d4916e364e
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-11-25 17:24:22 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,42 +53,42 @@ export default defineConfig({
 		icon(),
 	],
 
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				includeAssets: ["favicon.svg"],
-				workbox: {
-					globDirectory: "dist",
-					globPatterns: [
-						"**/*.{js,html,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
-					],
-					navigateFallback: "/",
-					runtimeCaching: [
-						{
-							urlPattern: ({ request }) => request.destination === "image",
-							handler: "CacheFirst", // Кэшируем изображения
-							options: {
-								cacheName: "image-cache",
-								expiration: {
-									maxEntries: 50,
-									maxAgeSeconds: 7 * 24 * 60 * 60, // Кэшируем на неделю
-								},
-							},
-						},
-					],
-				},
-				devOptions: {
-					enabled: import.meta.env.MODE === "development" ? true : false,
-					navigateFallbackAllowlist: [/^\//],
-				},
-				experimental: {
-					directoryAndTrailingSlashHandler: true,
-				},
-			}),
-		],
-	},
+	// git
+	// 	plugins: [
+	// 		VitePWA({
+	// 			registerType: "autoUpdate",
+	// 			manifest,
+	// 			includeAssets: ["favicon.svg"],
+	// 			workbox: {
+	// 				globDirectory: "dist",
+	// 				globPatterns: [
+	// 					"**/*.{js,html,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+	// 				],
+	// 				navigateFallback: "/",
+	// 				runtimeCaching: [
+	// 					{
+	// 						urlPattern: ({ request }) => request.destination === "image",
+	// 						handler: "CacheFirst", // Кэшируем изображения
+	// 						options: {
+	// 							cacheName: "image-cache",
+	// 							expiration: {
+	// 								maxEntries: 50,
+	// 								maxAgeSeconds: 7 * 24 * 60 * 60, // Кэшируем на неделю
+	// 							},
+	// 						},
+	// 					},
+	// 				],
+	// 			},
+	// 			devOptions: {
+	// 				enabled: import.meta.env.MODE === "development" ? true : false,
+	// 				navigateFallbackAllowlist: [/^\//],
+	// 			},
+	// 			experimental: {
+	// 				directoryAndTrailingSlashHandler: true,
+	// 			},
+	// 		}),
+	// 	],
+	// },
 
 	output: "hybrid",
 	adapter: cloudflare(),
